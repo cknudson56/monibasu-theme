90563db3698594412c44c58f53fedec6351eb8cb
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-07-02 13:48:20 -0500

    Take out comment in header to see if that fixes error

--- header.php
+++ header.php
@@ -13,5 +13,4 @@
 
   <header>
     <h1><a href="<?php echo bloginfo('url'); ?>"><?php echo bloginfo('name'); ?></a></h1>
-    <!-- Navigation when we learn how to add the dynamic nav -->
   </header>
