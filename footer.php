94d615e7925912f58c3eab87fdf5b99bd0a89d99
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-07-02 14:04:02 -0500

    Add h2 and h3 to footer

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
 <?php wp_footer(); ?>
 
 <footer>
-
+<!-- Navigation -->
+<h2><a href="<?php echo bloginfo('url'); ?>"><?php echo bloginfo('name'); ?></a></h2>
+<h3><?php echo bloginfo('description'); ?></h3>
+<!-- Social Media Icons -->
 </footer>
 
 </body>
