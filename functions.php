f782147fc84cea19bd4f97bdb84c251704fb6dae
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-07-05 21:56:43 -0500

    Add more styling to header

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ function example_theme_scripts(){
   //Skeleton
   wp_enqueue_style('skeleton', get_template_directory_uri() . '/css/skeleton.css');
   wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css');
-  //CSS
+  //CSS - NOT WORKING!!!
   wp_enqueue_style('main-styles', get_stylesheet_uri());
   //JS
   //wp_enqueue_style('script-name', get_template_directory_uri() . '/js/script.js');
