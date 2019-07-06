d3ddb408f23c4376c4d62fbf2ff41e718b80f4dc
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-07-05 21:42:40 -0500

    Link css in head

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php bloginfo('name'); ?></title>
-  <!--<link rel="stylesheet" type="text/css" href="<//?php bloginfo('stylesheet_url'); ?>" />-->
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
   <link href="https://fonts.googleapis.com/css?family=Montserrat:200,200i,400,400i,500,500i,600,600i,700,700i,800,800i|Playfair+Display:400,400i,700,700i,900,900i&display=swap" rel="stylesheet" />
   <?php wp_head(); ?>
 </head>
