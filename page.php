6d99e67e4231e10af6865fd3f7acdc2d0796e019
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-07-08 22:56:57 -0500

    Add initial structure to page.php

--- page.php
+++ page.php
@@ -1,5 +1,17 @@
 <?php get_header(); ?>
 
+<div class="container">
+  <div class="row">
 
+    <div class="one-half column">
+        <?php dynamic_sidebar(''); ?>
+    </div>
+
+    <div class="one-half column">
+      
+    </div>
+
+  </div>
+</div>
 
 <?php get_footer(); ?>
