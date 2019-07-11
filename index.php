af05bfd8c5845132d44c239367fddd62aef52710
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-07-10 23:05:35 -0500

    Add loop to index

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php get_header(); ?>
 
-
+<div class="container">
+  <div class="row">
+    <?php
+      if(have_posts()){
+        while(have_posts()){
+          the_post();?>
+          <div class="">
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <p><?php echo "Posted: " . get_the_date(); ?></p>
+            <?php the_post_thumbnail('medium'); ?>
+            <?php the_excerpt(); ?>
+          </div>
+        <?php
+      }
+    } ?>
+  </div>
+</div>
 
 <?php get_footer(); ?>
