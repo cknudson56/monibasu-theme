3eee92a335d5531c27e4cf7889f55a5f307ddc64
Christie L Knudson <christie.knudson@christies-mbp.lan>
2019-07-16 10:42:13 -0500

    Add archive page

---
+++ archive.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<div class="container content-margin">
+  <div class="row">
+    <div class="twelve columns">
+      <?php
+        if(have_posts()){
+          while(have_posts()){
+            the_post();?>
+            <div class="twelve columns">
+              <?php the_post_thumbnail('medium'); ?>
+              <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+              <p><?php echo "Posted: " . the_time('F j, Y'); ?></p>
+              <?php the_excerpt(); ?>
+              </p><a href="<?php the_permalink(); ?>">Read More...</a></p>
+            </div>
+          <?php
+        }
+      } ?>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
