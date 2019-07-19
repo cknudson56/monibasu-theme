ba82d16d74eef95a0fb50d4a60faea5d24d5cda3
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2019-07-18 21:26:48 -0500

    Adjust blog template

--- category.php
+++ category.php
@@ -18,13 +18,15 @@
         if(have_posts()){
           while(have_posts()){
             the_post();?>
-              <div class="six columns">
-              <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
-              <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-              <?php publish_date(); ?>
-              <?php the_excerpt(); ?>
-              </p><a href="<?php the_permalink(); ?>">Read More...</a></p>
-            </div>
+              <div class="three columns">
+                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+              </div>
+              <div class="nine columns">
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                <?php publish_date(); ?>
+                <?php the_excerpt(); ?>
+                </p><a href="<?php the_permalink(); ?>">Read More...</a></p>
+              </div>
             <?php
           }
         }
