e0e5d1d1e628c736807b63c62f7fa3577db26eb4
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2019-07-18 22:52:53 -0500

    Change archive and date page style

--- archive.php
+++ archive.php
@@ -2,28 +2,40 @@
 
 <div id="archives-page" class="container content-margin">
   <div class="row">
+    <div class="twelve columns cat-description">
+      <h2><?php single_cat_title(); ?></h2>
+      <?php echo category_description(); ?>
+    </div>
+  </div>
+  <div class="row">
+    <div class="sidebar-inline twelve columns">
+      <?php get_sidebar(); ?>
+    </div>
+  </div>
+  <div class="row">
     <div class="twelve columns">
       <?php
         if(have_posts()){
           while(have_posts()){
             the_post();?>
-            <div class="nine columns">
-              <?php the_post_thumbnail('medium'); ?>
-              <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-              <?php publish_date(); ?>
-              <?php the_excerpt(); ?>
-              </p><a href="<?php the_permalink(); ?>">Read More...</a></p>
+            <div class="row category-flex category-border">
+              <div class="">
+                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+              </div>
+              <div class="category-post">
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                <?php publish_date(); ?>
+                <?php the_excerpt(); ?>
+                </p><a href="<?php the_permalink(); ?>">Read More...</a></p>
+              </div>
             </div>
-          <?php
+            <?php
+          }
         }
-      } ?>
-    </div>
-
-    <div class="three columns">
-      <?php get_sidebar(); ?>
-
+      ?>
     </div>
   </div>
 </div>
+
 
 <?php get_footer(); ?>
--- date.php
+++ date.php
@@ -17,25 +17,28 @@
    </div>
 
    <div class="row">
-     <div class="nine columns">
-        <?php
-          if(have_posts()){
-            while(have_posts()){
-              the_post();?>
-                <?php the_post_thumbnail('medium'); ?>
-                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                <?php publish_date(); ?>
-                <?php the_excerpt(); ?>
-                </p><a href="<?php the_permalink(); ?>">Read More...</a></p>
-            <?php
-          }
-        } ?>
-      </div>
-
-    <div class="three columns">
-      <?php get_sidebar(); ?>
-    </div>
-  </div>
+     <div class="twelve columns">
+       <?php
+         if(have_posts()){
+           while(have_posts()){
+             the_post();?>
+             <div class="row category-flex category-border">
+               <div class="">
+                 <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+               </div>
+               <div class="category-post">
+                 <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                 <?php publish_date(); ?>
+                 <?php the_excerpt(); ?>
+                 </p><a href="<?php the_permalink(); ?>">Read More...</a></p>
+               </div>
+             </div>
+             <?php
+           }
+         }
+       ?>
+     </div>
+   </div>
 
 </div>
 
