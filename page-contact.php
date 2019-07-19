17de271804989dea9cabc6eb6a801f27621cb0cb
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2019-07-19 14:34:20 -0500

    Adjust contact page classes and style

--- page-contact.php
+++ page-contact.php
@@ -1,18 +1,18 @@
 <?php /*Template Name: Contact Page Template*/ ?>
 <?php get_header(); ?>
 
-<div id="contact-page" class="container content-margin">
+<div id="contact-page" class="container content-margin-contact">
 
   <div class="row">
     <h2><?php the_title(); ?></h2>
   </div>
 
-  <div class="row">
-    <div class="one-half column">
+  <div class="row contact-flex">
+    <div class="one-half column left-contact-column">
         <?php dynamic_sidebar('left-contact-page'); ?>
     </div>
 
-    <div class="one-half column">
+    <div class="one-half column right-contact-column">
       <?php
         if(have_posts()){
           while(have_posts()){
