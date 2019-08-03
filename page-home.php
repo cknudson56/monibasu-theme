77999284c32060ae3fb52984506ef7d86e575c62
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-08-03 16:42:36 -0500

    Change the stucture of the home page

--- page-home.php
+++ page-home.php
@@ -2,36 +2,23 @@
 <?php get_header(); ?>
 
 <div id="home-page" class="container">
+
   <div class="row">
-
     <div class="one-half column">
-      <div class="row">
         <?php dynamic_sidebar('top-left-home'); ?>
-      </div>
-
-      <div class="row">
-        <!--
-        <div class="three columns">
-          <//?php dynamic_sidebar('left-social'); ?>
-        </div>
-        <div class="three columns">
-          <//?php dynamic_sidebar('right-social'); ?>
-        </div>
-        -->
-
-        <div>
-          <?php dynamic_sidebar('main-social'); ?>
-        </div>
-
-      </div>
-
     </div>
 
     <div class="one-half column">
       <?php dynamic_sidebar('top-right-home'); ?>
     </div>
-
   </div>
+
+  <div class="row">
+    <div>
+      <?php dynamic_sidebar('main-social'); ?>
+    </div>
+  </div>
+  
 </div>
 
 
