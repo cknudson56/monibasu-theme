3a91dc5cfc627c33e248fa3623ddba04ce73f743
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-08-04 12:56:13 -0500

    Add more archive sections.

--- archive.php
+++ archive.php
@@ -2,9 +2,23 @@
 
 <div id="archives-page" class="container content-margin">
   <div class="row">
-    <div class="twelve columns cat-description">
-      <h2><?php single_cat_title(); ?></h2>
-      <?php echo category_description(); ?>
+    <div class="twelve columns">
+      <h2><?php
+        if(is_category()){
+          single_cat_title();
+        }elseif(is_tag()){
+          single_tag_title();
+        }elseif(is_day()){
+          echo "Daily Archives: " . get_the_date();
+        }elseif(is_month()){
+          echo "Monthly Archives: " . get_the_date('F Y');
+        }elseif(is_year()){
+          echo "Yearly Archives: " . get_the_date('Y');
+        }else{
+          echo "Archives";
+        }
+       ?>
+      </h2>
     </div>
   </div>
   <div class="row">
