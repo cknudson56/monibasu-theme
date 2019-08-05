e6a4f08b3ee88012b0e1d72ed720620036870a18
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-08-05 12:41:50 -0500

    Add more stling to comments

--- comments.php
+++ comments.php
@@ -2,10 +2,12 @@
 
 foreach ($comments as $comment): ?>
   <div class="comments"><?php
-    echo '<h3>Comments</h3>';
-    echo '<div class="comment-author">' . get_comment_author_link() . '</div>';
-    echo '<div class="comment-date">' . get_comment_date() . '</div>';
-    echo '<div class="comment-text">' . get_comment_text() . '</div>';?>
+    echo '<h3>Comments</h3>';?>
+    <div class="comments-items category-border"><?php
+      echo '<div class="comment-author">' . get_comment_author_link() . '</div>';
+      echo '<div class="comment-date">' . get_comment_date() . '</div>';
+      echo '<div class="comment-text">' . get_comment_text() . '</div>';?>
+    </div>
   </div><?php
 endforeach;
 
