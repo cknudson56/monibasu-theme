9008f87872fd32b5e725135da50068d1bd232e0b
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-08-05 12:43:47 -0500

    Fix comments classes

--- comments.php
+++ comments.php
@@ -1,13 +1,10 @@
 <?php
-
+  echo '<h3>Comments</h3>';
 foreach ($comments as $comment): ?>
-  <div class="comments"><?php
-    echo '<h3>Comments</h3>';?>
-    <div class="comments-items category-border"><?php
-      echo '<div class="comment-author">' . get_comment_author_link() . '</div>';
-      echo '<div class="comment-date">' . get_comment_date() . '</div>';
-      echo '<div class="comment-text">' . get_comment_text() . '</div>';?>
-    </div>
+  <div class="comments-items category-border"><?php
+    echo '<div class="comment-author">' . get_comment_author_link() . '</div>';
+    echo '<div class="comment-date">' . get_comment_date() . '</div>';
+    echo '<div class="comment-text">' . get_comment_text() . '</div>';?>
   </div><?php
 endforeach;
 
