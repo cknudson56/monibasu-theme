6b8a4e6fe35ea7d72df2c528086ae35de101e030
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-08-05 12:31:13 -0500

    Edits to comments

--- comments.php
+++ comments.php
@@ -2,9 +2,10 @@
 
 foreach ($comments as $comment): ?>
   <div class="comments"><?php
-    echo '<div class="comment-author">' . get_comment_author_link() . '</div>';
-    echo '<div class="comment-date">' . get_comment_date() . '</div>';
-    echo '<div class="comment-text">' . get_comment_text() . '</div>';?>
+    echo '<h3>Comments</h3>'
+    echo '<div class="comment-author"><h5>' . get_comment_author_link() . '</h5></div>';
+    echo '<div class="comment-date"><p>' . get_comment_date() . '</p></div>';
+    echo '<div class="comment-text"><p>' . get_comment_text() . '</p></div>';?>
   </div><?php
 endforeach;
 
