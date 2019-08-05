c62fb2681f2339df9effcdc443dc223fc9c851d1
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-08-05 12:31:46 -0500

    Fix error

--- comments.php
+++ comments.php
@@ -2,7 +2,7 @@
 
 foreach ($comments as $comment): ?>
   <div class="comments"><?php
-    echo '<h3>Comments</h3>'
+    echo '<h3>Comments</h3>';
     echo '<div class="comment-author"><h5>' . get_comment_author_link() . '</h5></div>';
     echo '<div class="comment-date"><p>' . get_comment_date() . '</p></div>';
     echo '<div class="comment-text"><p>' . get_comment_text() . '</p></div>';?>
