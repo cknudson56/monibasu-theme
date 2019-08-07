763e7cef71734f9adf7481ed8f8746a81faadfbe
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-08-07 18:47:34 -0500

    Add Favicon

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
   <title><?php bloginfo('name'); ?></title>
   <!--<link rel="stylesheet" type="text/css" href="<//?php bloginfo('stylesheet_url'); ?>" />-->
   <link href="https://fonts.googleapis.com/css?family=Cardo:400,400i,700|Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
   <?php wp_head(); ?>
 </head>
 
