76e98b52a0822a7051557e0ea6fcb84d8daa4b51
Christie L Knudson <christie_knudson@Christies-iMac.local>
2019-08-07 13:02:00 -0500

    See if  css link is working

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php bloginfo('name'); ?></title>
-  <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+  <!--<link rel="stylesheet" type="text/css" href="<//?php bloginfo('stylesheet_url'); ?>" />-->
   <link href="https://fonts.googleapis.com/css?family=Cardo:400,400i,700|Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
   <?php wp_head(); ?>
 </head>
